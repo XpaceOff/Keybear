82bcb8fda8186a98f3591e1cebfbcab49b0b84bf
xoffio <marquessricardo@gmail.com>
2023-10-08 11:35:40 -0400

    initial code for blok. Left pcb working with under leds

---
+++ firmware/kmk/blok/code.py
@@ -0,0 +1,32 @@
+print("Starting")
+
+from kb import KMKKeyboard
+from kmk.keys import KC
+
+from kmk.modules.split import Split, SplitSide, SplitType
+from kmk.extensions.RGB import RGB
+
+keyboard = KMKKeyboard()
+
+# Adding extensions
+rgb = RGB(pixel_pin=keyboard.rgb_pixel_pin, num_pixels=6)
+
+# TODO Comment one of these on each side
+split_side = SplitSide.LEFT
+#split_side = SplitSide.RIGHT
+split = Split(split_type=SplitType.BLE, split_side=split_side)
+
+keyboard.modules.append(split)
+keyboard.extensions.append(rgb)
+
+keyboard.keymap = [
+    [
+        KC.TAB,    KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,                    KC.Y,    KC.U,    KC.I,    KC.O,   KC.P,  KC.BSPC,\
+        KC.CLCK,   KC.A,    KC.S,    KC.D,    KC.F,    KC.G,                    KC.H,    KC.J,    KC.K,    KC.L, KC.SCLN, KC.QUOT,\
+        KC.LSFT,   KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,                    KC.N,    KC.M, KC.COMM,  KC.DOT, KC.SLSH, KC.RSFT,\
+        KC.LGUI,   KC.LCTL, KC.B,    KC.SPC,  KC.X,    KC.X,                    KC.LGUI, KC.B,    KC.B,    KC.X,   KC.X,  KC.X,
+    ]
+]
+
+if __name__ == '__main__':
+    keyboard.go()
---
+++ firmware/kmk/blok/kb.py
@@ -0,0 +1,20 @@
+import board
+
+from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
+from kmk.scanners import DiodeOrientation
+
+class KMKKeyboard(_KMKKeyboard):
+    col_pins = (board.GP29, board.GP28, board.GP27, board.GP26, board.GP22, board.GP20,)
+    row_pins = (board.GP05, board.GP06, board.GP07, board.GP08,)
+    diode_orientation = DiodeOrientation.COLUMNS
+
+    rgb_pixel_pin = board.TX
+
+    # flake8: noqa
+    # fmt: off
+    coord_mapping = [
+     0,  1,  2,  3,  4,  5,  29, 28, 27, 26, 25, 24,
+     6,  7,  8,  9, 10, 11,  35, 34, 33, 32, 31, 30,
+    12, 13, 14, 15, 16, 17,  41, 40, 39, 38, 37, 36,
+    18, 19, 20, 21, 22, 23,  47, 46, 45, 44, 43, 42,
+    ]
