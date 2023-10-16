6e39fa6c428c4988feccf31c663f3244ca66bf4a
xoffio <marquessricardo@gmail.com>
2023-10-15 21:18:48 -0400

    testing adding layers

--- firmware/kmk/blok/code.py
+++ firmware/kmk/blok/code.py
@@ -1,6 +1,7 @@
 from kb import KMKKeyboard
 
 from kmk.keys import KC
+from kmk.modules.layers import Layers
 from kmk.modules.split import Split, SplitSide, SplitType
 from kmk.modules.encoder import EncoderHandler
 from kmk.extensions.RGB import RGB
@@ -18,6 +19,7 @@ split = Split(
     use_pio=True,
 )
 keyboard.modules.append(split)
+keyboard.modules.append(Layers())
 
 encoder_handler = EncoderHandler()
 encoder_handler.pins = ((keyboard.encoder_pin_a, keyboard.encoder_pin_b, None, False),)
@@ -26,13 +28,27 @@ encoder_handler.pins = ((keyboard.encoder_pin_a, keyboard.encoder_pin_b, None, F
 rgb = RGB(pixel_pin=keyboard.rgb_pixel_pin, num_pixels=29, hue_default=190)
 keyboard.extensions.append(rgb)
 
+# Cleaner key names
+_______ = KC.TRNS
+XXXXXXX = KC.NO
+
+LOWER = KC.MO(1)
+RAISE = KC.MO(2)
+
 keyboard.keymap = [
     [
-        KC.TAB,    KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,                    KC.Y,    KC.U,    KC.I,    KC.O,   KC.P,  KC.BSPC,\
-        KC.CLCK,   KC.A,    KC.S,    KC.D,    KC.F,    KC.G,                    KC.H,    KC.J,    KC.K,    KC.L, KC.SCLN, KC.QUOT,\
-        KC.LSFT,   KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,                    KC.N,    KC.M, KC.COMM,  KC.DOT, KC.SLSH, KC.RSFT,\
-        KC.LGUI,   KC.LCTL, KC.B,    KC.SPC,  KC.X,    KC.RGB_MODE_BREATHE_RAINBOW,      KC.LGUI, KC.B,    KC.B,    KC.X,   KC.X,  KC.X,
+        KC.TAB,    KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,                KC.Y,    KC.U,    KC.I,    KC.O,  KC.P,    KC.BSPC,\
+        KC.CLCK,   KC.A,    KC.S,    KC.D,    KC.F,    KC.G,                KC.H,    KC.J,    KC.K,    KC.L,  KC.SCLN, KC.QUOT,\
+        KC.LSFT,   KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,                KC.N,    KC.M, KC.COMM,  KC.DOT,  KC.SLSH, KC.RSFT,\
+        KC.LGUI,   KC.LCTL, LOWER,   KC.SPC,  KC.X,    KC.X,                KC.LGUI, KC.B,    KC.B,    KC.X,  KC.X,    KC.X,
+    ],
+    [ # LOWER
+        KC.TAB,    KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,               KC.N6,   KC.N7,   KC.N8,   KC.N9, KC.N0,   KC.BSPC,\
+        KC.CLCK,   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                KC.H,    KC.J,    KC.K,    KC.L,  KC.SCLN, KC.QUOT,\
+        KC.LSFT,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                KC.N,    KC.M, KC.COMM,  KC.DOT,  KC.SLSH, KC.RSFT,\
+        KC.LGUI,   KC.LCTL, LOWER,   KC.SPC,  KC.X,    KC.X,                KC.LGUI, KC.B,    KC.B,    KC.X,   KC.X,  KC.X,
     ]
+
 ]
 
 encoder_handler.map = (
