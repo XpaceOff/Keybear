821352f172c316325400869e50fdcb5c432c22e1
xoffio <marquessricardo@gmail.com>
2023-10-19 23:01:15 -0400

    small firware changes.. RGB_TOG bug present

--- firmware/kmk/blok/code.py
+++ firmware/kmk/blok/code.py
@@ -6,9 +6,11 @@ from kmk.modules.holdtap import HoldTap
 from kmk.modules.split import Split, SplitSide, SplitType
 from kmk.modules.encoder import EncoderHandler
 from kmk.modules.cg_swap import CgSwap
-from kmk.extensions.RGB import RGB
+from kmk.extensions.rgb import RGB
 
 keyboard = KMKKeyboard()
+
+#keyboard.debug_enabled = True
 
 # TODO Comment one of these on each side
 # split_side = SplitSide.LEFT
@@ -35,7 +37,7 @@ cg_swap = CgSwap()
 keyboard.modules.append(cg_swap)
 
 # Adding extensions
-rgb = RGB(pixel_pin=keyboard.rgb_pixel_pin, num_pixels=29, hue_default=190, val_default=100)
+rgb = RGB(pixel_pin=keyboard.rgb_pixel_pin, num_pixels=29, hue_default=190, val_default=75)
 keyboard.extensions.append(rgb)
 
 # Cleaner key names
@@ -44,9 +46,9 @@ XXXXXXX = KC.NO
 TBD_KEY = KC.NO # This key will be removed next hardware update.
 
 DFT_L = KC.TO(0)                    # Default Layer: set default layer. 
-DFT_SL = KC.HT(KC.TO(0), KC.LSFT)   # Default Shift Layer: Shift when pressed, and set default layer when tapped.
-LRS_SL = KC.HT(KC.TG(1), KC.LSFT)   # Lower Shift Layer: Shift when pressed, and set lower layer when tapped.
-RSE_L = KC.MO(2)
+DFT_LS = KC.HT(KC.TO(0), KC.LSFT)   # Default Layer&Shift: Shift when pressed, and set default layer when tapped.
+LRS_LS = KC.HT(KC.TG(1), KC.LSFT)   # Lower Layer&Shift: Shift when pressed, and set lower layer when tapped.
+RSE_L = KC.MO(2)                    # Raise Layer: set raise layer when pressed.
 CFG_L = KC.MO(3)                    # Config Layer: set config layer when pressed.
 
 RGB_TG = KC.RGB_TOG
@@ -67,13 +69,13 @@ keyboard.keymap = [
         KC.TAB,    KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,                KC.Y,    KC.U,    KC.I,    KC.O,     KC.P,    KC.BSPC,\
         KC.CLCK,   KC.A,    KC.S,    KC.D,    KC.F,    KC.G,                KC.H,    KC.J,    KC.K,    KC.L,     KC.SCLN, KC.QUOT,\
         KC.LSFT,   KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,                KC.N,    KC.M,    KC.COMM, KC.DOT,   KC.SLSH, KC.ESC,\
-        KC.LGUI,   KC.LCTL, LRS_SL,  KC.SPC,  TBD_KEY, ENC_LB0,             ENC_RB0, TBD_KEY, KC.ENTER,RSE_L,    KC.RALT, KC.X,
+        KC.LGUI,   KC.LCTL, LRS_LS,  KC.SPC,  TBD_KEY, ENC_LB0,             ENC_RB0, TBD_KEY, KC.ENTER,RSE_L,    KC.RALT, KC.X,
     ],
     [ # LOWER LAYER
         KC.TAB,    KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,               KC.N6,   KC.N7,   KC.N8,   KC.N9,    KC.N0,   KC.BSPC,\
         _______,   _______, _______, _______, _______, _______,             KC.LEFT, KC.DOWN, KC.UP,   KC.RIGHT, _______, _______,\
         _______,   _______, _______, _______, _______, _______,             _______, _______, _______, _______,  _______, _______,\
-        _______,   _______, DFT_SL,  _______, TBD_KEY, ENC_LB1,             ENC_RB0, TBD_KEY, _______, DFT_L,    _______, KC.X,
+        _______,   _______, DFT_LS,  _______, TBD_KEY, ENC_LB1,             ENC_RB0, TBD_KEY, _______, DFT_L,    _______, KC.X,
     ],
     [ # RAISE LAYER
         KC.TILD,   KC.EXLM, KC.AT,   KC.HASH, KC.DLR,  KC.PERC,             KC.CIRC, KC.AMPR, KC.ASTR, KC.LPRN,  KC.RPRN, KC.BSPC,\
@@ -82,10 +84,10 @@ keyboard.keymap = [
         _______,   _______, CFG_L,   _______, TBD_KEY, KC.CG_TOGG,          ENC_RB0, TBD_KEY, _______, XXXXXXX,  _______, KC.X,
     ],
     [ # CONFIG LAYER
-        _______,   _______, _______, _______, _______,  RGB_TG,            _______, _______, _______, _______,  _______, _______,\
-        _______,   _______, _______, RGB_SU,  RGB_TG,  KC.R,               _______, _______, _______, _______,  _______, _______,\
-        _______,   _______, _______, RGB_SD,  RGB_BD,  _______,            _______, _______, _______, _______,  _______, _______,\
-        _______,   _______, XXXXXXX, _______, TBD_KEY, KC.CG_TOGG,         _______, _______, _______, _______,  _______, _______,\
+        _______,   _______, _______, _______, _______, RGB_TG,              RGB_TG, _______, _______, _______,  _______, _______,\
+        _______,   _______, _______, RGB_SU,  RGB_BU,  KC.R,                _______, _______, _______, _______,  _______, _______,\
+        _______,   _______, _______, RGB_SD,  RGB_BD,  _______,             _______, _______, _______, _______,  _______, _______,\
+        _______,   _______, XXXXXXX, _______, TBD_KEY, KC.CG_TOGG,          _______, _______, _______, _______,  _______, _______,\
     ]
 ]
 
